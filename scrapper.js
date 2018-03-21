5bb02329e83b57513bc6aa8edd66808e5026da2b
Mahdan Ahmad Fauzi Al-Hasan <mahdan.ahmad@yahoo.com>
2018-03-21 23:20:03 +0700

    working key

--- scrapper.js
+++ scrapper.js
@@ -5,18 +5,19 @@ const projectId = "hardy-notch-196607";
 // Creates a client
 const bigquery = new BigQuery({
 	projectId: projectId,
+	keyFilename: 'key.json'
 });
 
-let query	= "SELECT" +
-	"V2Locations, V2Themes, DocumentIdentifier, DATE" +
-"FROM" +
-	"[gdelt-bq:gdeltv2.gkg]" +
-"WHERE" +
-	"V2Locations LIKE '%Indonesia%'" +
-	"AND DATE >= 20160132000000" +
-	"AND DATE < 20160133000000";
+let query	= "SELECT " +
+	"V2Locations, V2Themes, DocumentIdentifier, DATE " +
+"FROM " +
+	"[gdelt-bq:gdeltv2.gkg] " +
+"WHERE " +
+	"V2Locations LIKE '%Indonesia%' " +
+	"AND DATE >= 20160114000000 " +
+	"AND DATE < 20160115000000";
 
-let options	= { query, useLegacySql: false };
+let options	= { query, useLegacySql: true };
 
 bigquery
 	.query(options)
